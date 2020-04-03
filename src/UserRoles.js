bdd1317cb48af61b70f201173f09e431a78ae7dd
jusufdelalic <jdelalic1@etf.unsa.ba>
2020-04-03 17:27:55 +0200

    Mogućnost brisanja user roles

--- src/UserRoles.js
+++ src/UserRoles.js
@@ -1,24 +1,25 @@
-import { Descriptions, Badge, Table, Button } from 'antd';
+import { Table, Button } from 'antd';
 import React,{useState,useEffect} from 'react';
 import {AuthService} from './AuthService';
 
 function UserRoles(props){
 
+    const [currentUser, setCurrentUser] = useState({
+        userId:"",
+        username:"",
+        email:"",
+        name:"",
+        surname:"",
+        address:"",
+        phoneNumber:"",
+        city:"",
+        country:"",
+        roles:[{}] // added empty array for roles
+    });
 
     useEffect(()=>{getUser();},[]);
     
-    const [currentUser, setCurrentUser] = useState({
-                                            userId:"",
-                                            username:"",
-                                            email:"",
-                                            name:"",
-                                            surname:"",
-                                            address:"",
-                                            phoneNumber:"",
-                                            city:"",
-                                            country:"",
-                                            roles:[{}] // added empty array for roles
-                                        });
+
 
 
 async function  getUser()  {
@@ -29,7 +30,6 @@ async function  getUser()  {
         method: 'GET',
         headers: myHeaders,
     };
-    //const data = await fetch("https://localhost:8080/api/users", requestOptions);
     const data = await fetch("https://main-server-si.herokuapp.com/api/users", requestOptions);
     const users = await data.json();
     users.map((x)=>{
@@ -38,49 +38,43 @@ async function  getUser()  {
     })
 }
 
-let roles = currentUser.roles;
+
 let username = currentUser.username;
 
 let deletedRoles = [];
 let addedRoles = [];
+let filterRoles = currentUser.roles;
 
 
-
+const [roles, setRoles] = useState(currentUser.roles);
 
 
 function deleteTableRow(e, row) {
 
+
+    if(e!= null && row!=null) {
+
     e.preventDefault();
-    console.log("Delete the role which has the Id = " + row.id);
+    console.log("Delete the role " + JSON.stringify(row));
 
-    // somethings wrong...
+    filterRoles = currentUser.roles.filter((value, index, array) => value != row);
+    
+    console.log("filter = " + JSON.stringify(filterRoles));
 
-    deletedRoles.concat(row);
+    setRoles(filterRoles);
+    currentUser.roles = filterRoles;
 
-    var array = [...roles];
-    var index = array.indexOf(row)
-    if (index !== -1) {
-        array.splice(index, 1);
-        //this.setState({roles: array});
-      }
+    }
 
+    else {
+        console.log("Error deleting table row!")
+    }
 
 }
 
 
 
-
-
-
-
-
 const columns = [
-    {
-      title: 'Role Id',
-      dataIndex: 'id', // dataIndex = attribute name in json data source
-      key: 'roleid', 
-      render: text => <a>{text}</a>
-    },
     {
       title: 'Role Name',
       dataIndex: 'rolename',
@@ -95,47 +89,19 @@ const columns = [
 ];
 
 
-
-
 return (  
 
     <div>
 
     <h2>{username} Roles</h2>
 
-    <Table columns={columns} dataSource={roles}/>
+    <Table columns={columns} dataSource={currentUser.roles}/>
 
     </div>
    
 );
    
-    /*return (
-        
-        
-         <Descriptions title={username.concat(" Roles")} bordered>
-          <Descriptions.Item label="Id">{currentUser.userId}</Descriptions.Item>
-          <Descriptions.Item label="Name">{currentUser.name}</Descriptions.Item>
-          <Descriptions.Item label="Surame">{currentUser.surname}</Descriptions.Item>
-          <Descriptions.Item label="Username">{currentUser.username}</Descriptions.Item>
-          <Descriptions.Item label="Address">{currentUser.address}</Descriptions.Item>
-          <Descriptions.Item label="City">{currentUser.city}</Descriptions.Item>
-          <Descriptions.Item label="Country">{currentUser.country}</Descriptions.Item>
-          <Descriptions.Item label="Phone Number">{currentUser.phoneNumber}</Descriptions.Item>
-          <Descriptions.Item label="Email">{currentUser.email}</Descriptions.Item>
-          <Descriptions.Item label="Role No1">{roles[0].rolename}</Descriptions.Item>
-          {console.log("Roles = " + JSON.stringify(roles))};
-          
- 
-        </Descriptions> 
-    )
-    */
-
-    /*return <div>
-        <h1>
-            {props.match.params.id}
-        </h1>
-        </div>*/
-        
+    
 }
 
 export default UserRoles;
