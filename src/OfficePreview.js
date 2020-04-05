2a69159d926babef4751bb88d6d676d8d80b686f
nsisic1 <37252626+nsisic1@users.noreply.github.com>
2020-04-05 15:24:01 +0200

    Prikaz kase (#25)
    * Dodana tabela Cash Registers
    
    * Dodao komponente tabele id, name i QR kod placeholder

--- src/OfficePreview.js
+++ src/OfficePreview.js
@@ -1,5 +1,5 @@
-import { Descriptions,Badge,Table } from 'antd';
-import React,{useState,useEffect} from 'react'
+import { Descriptions, Badge, Table, Button } from 'antd';
+import React,{useState, useEffect} from 'react'
 import {AuthService} from './AuthService'
 import {Link} from 'react-router-dom'
 
@@ -37,8 +37,57 @@ function OfficePreview({match}){
                 setCurrentOffice(x);
         })
     }
+	
+	const dataSource = [
+	  {
+		key: '1',
+		name: 'Mike',
+		age: 32,
+		address: '10 Downing Street',
+	  },
+	  {
+		key: '2',
+		name: 'John',
+		age: 42,
+		address: '10 Downing Street',
+	  },
+	];
+
+	const columns = [
+	  {
+		title: 'id',
+		dataIndex: 'id',
+		key: 'id',
+	  },
+	  {
+		title: 'name',
+		dataIndex: 'name',
+		key: 'name',
+	  },
+	   {
+		title: 'View QR code',
+        key: 'qr',
+		render: (text, register)=>{
+          //register je office
+           return <Button type={register ? "primary":"disabled"}> 
+             View QR code
+           </Button>
+        },
+	   },
+	   {
+		title: 'Delete Cash Register',
+        key: 'delete',
+		render: (text, register)=>{
+          //register je office
+           return <Button type={register ? "primary":"disabled"} danger> 
+             Delete Cash Register
+           </Button>
+        },
+	   },
+	];
    
     return (
+		<div>
         <Descriptions title="Office Info" bordered>
           <Descriptions.Item label="Id">{currentOffice.id}</Descriptions.Item>
           <Descriptions.Item label="Address">{currentOffice.address}</Descriptions.Item>
@@ -48,7 +97,17 @@ function OfficePreview({match}){
           <Descriptions.Item label="Email">{currentOffice.email}</Descriptions.Item>
           <Descriptions.Item label="Number of Cash Registers">{currentOffice.cashRegisters.length}</Descriptions.Item>
           <Descriptions.Item span={2} label="Manager">{`${currentOffice.manager.name} ${currentOffice.manager.surname}`}</Descriptions.Item>
-        </Descriptions> 
+		  
+        </Descriptions>
+		
+		<div>{currentOffice.cashRegisters.length > 0 ?
+			<div>
+				<h3>Cash Registers</h3>
+				<Table columns={columns} dataSource={currentOffice.cashRegisters} pagination={false} locale={{emptyText: " ",}}/> 
+			</div>
+			: null}			
+		</div>
+		</div>
     )
 }
 export default OfficePreview;
