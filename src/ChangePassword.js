ab3ab5e569314f58810f1425bc4ae8a981249517
akocanovic1 <37149928+akocanovic1@users.noreply.github.com>
2020-04-05 16:17:55 +0200

    Dodan Change passoword za Admina (#29)
    Change passoword za Admina

--- src/ChangePassword.js
+++ src/ChangePassword.js
@@ -1,13 +1,88 @@
 import React from  'react'
+import { Form, Input, Button ,Switch} from 'antd';
+import {AuthService} from './AuthService'
 
-function ChangePassword(){
+
+function ChangePassword(props){
+
+    const layout = {labelCol: {span: 13,},wrapperCol: {span: 26,},};
+    const validateMessages = {required: 'This field is required!'};
+    const onFinish= (values)=>{
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization",AuthService.currentHeaderValue);
+        console.log(props.match.params.id);
+        if(values.password==values.confirm){
+        var raw = JSON.stringify({
+        "password":values.password,
+    });}
+    var requestOptions = {
+      method: 'PUT',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("https://main-server-si.herokuapp.com/api/auth/changePassword" /*+ props.match.params.id*/, requestOptions)
+      .then(response => response.json())
+      .then(result => {
+          console.log(result)
+        if(values.password==values.confirm){
+            console.log("Top")
+            props.history.push("/");
+        }else{
+         console.log("Error")
+        }
+      }) 
+  };
 
 
-
-    return <div>
+    return <Form onFinish={onFinish} validateMessages={validateMessages}>
+    <div>
         <h1>ChangePassword
         </h1>
+        <Form.Item
+        name="password"
+        label="Password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+            min:6,message:"Password must be at least 6 characters",
+          },
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+        <Form.Item
+        name="confirm"
+        label="Confirm Password"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: 'Please confirm your password!',
+          },
+          ({ getFieldValue }) => ({
+            validator(rule, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+
+              return Promise.reject('The two passwords that you entered do not match!');
+            },
+          }),
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+        <Button type="primary" htmlType="submit">
+        Submit
+        </Button>
         </div>
+        </Form>
 }
 
 export default ChangePassword;
